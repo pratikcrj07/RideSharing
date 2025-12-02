5fa87486a9643e9ecc7f1d7d4818897f42221a1b
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-02 23:40:17 +0545

    added OTPrequest

---
+++ src/main/java/com/ridesharing/DTOs/OtpRequest.java
@@ -0,0 +1,11 @@
+package com.ridesharing.DTOs;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class OtpRequest {
+    @NotBlank @Email
+    private String email;
+}
