8e12ecb1ef7df8378873881a2d34b6e393d06460
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-02 23:37:11 +0545

    added RegisterRequest DTO

---
+++ src/main/java/com/ridesharing/DTOs/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.ridesharing.DTOs;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotBlank private String name;
+    @NotBlank @Email private String email;
+    private String phone;
+    @NotBlank private String password;
+}
