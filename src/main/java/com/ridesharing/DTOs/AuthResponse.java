86765e685f36733c3768cdd76f43e0a64e828e37
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-02 23:39:21 +0545

    added Auth Response

---
+++ src/main/java/com/ridesharing/DTOs/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.ridesharing.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
+}
