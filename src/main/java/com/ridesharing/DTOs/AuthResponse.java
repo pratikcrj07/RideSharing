7c955cbf2217cfa97b10ba82d45625debd8015ab
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-04 23:12:35 +0545

    added refresh token constructor

--- src/main/java/com/ridesharing/DTOs/AuthResponse.java
+++ src/main/java/com/ridesharing/DTOs/AuthResponse.java
@@ -2,11 +2,18 @@ package com.ridesharing.DTOs;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
     private String accessToken;
     private String refreshToken;
     private String tokenType = "Bearer";
+
+    public AuthResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.tokenType = "Bearer";
+    }
 }
