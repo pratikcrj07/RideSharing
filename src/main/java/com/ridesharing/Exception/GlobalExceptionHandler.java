3ac0029edd84a435329c2b24f26235fea05d745c
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-05 23:33:18 +0545

    created ErrorResponse for Global ExceptionHandler

--- src/main/java/com/ridesharing/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/ridesharing/Exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.ridesharing.Exception;
 
-import com.nimbusds.oauth2.sdk.ErrorResponse;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,22 +8,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 
 
-    public class GlobalExceptionHandler {
+public class GlobalExceptionHandler {
 
-        @ExceptionHandler(ApiException.class)
-        public ResponseEntity<?> handleApi(ApiException ex) {
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<?> handleApi(ApiException ex) {
 
-            return ResponseEntity.badRequest().body(
-                    new ErrorResponse(400, ex.getMessage())
-            );
-        }
-
-        @ExceptionHandler(Exception.class)
-        public ResponseEntity<?> handleOther(Exception ex) {
-            return ResponseEntity.internalServerError().body(
-                    new ErrorResponse(500, "Something went wrong")
-            );
-        }
+        return ResponseEntity.badRequest().body(
+                new ErrorResponse(400, ex.getMessage())
+        );
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleOther(Exception ex) {
+        return ResponseEntity.internalServerError().body(
+                new ErrorResponse(500, "Something went wrong")
+        );
+    }
 }
+
+
