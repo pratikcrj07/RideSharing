8b0d42094a05fb3b2182f8b45b054f910e9242a7
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-12 15:43:54 +0545

    updated driver controller with api

--- src/main/java/com/ridesharing/Controller/DriverController.java
+++ src/main/java/com/ridesharing/Controller/DriverController.java
@@ -1,11 +1,21 @@
 package com.ridesharing.Controller;
 
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ridesharing.Entities.DriverApplication;
+import com.ridesharing.Services.DriverApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api")
-
+@RequestMapping("/api/driver")
+@RequiredArgsConstructor
 public class DriverController {
+
+    private final DriverApplicationService driverService;
+
+    @PostMapping("/apply")
+    public ResponseEntity<String> apply(@RequestParam Long userId,
+                                        @RequestBody DriverApplication req) {
+        return ResponseEntity.ok(driverService.apply(userId, req));
+    }
 }
