e9d69854f35e5c582c9b84a303089991a26933c2
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-24 21:49:37 +0545

    fixed kafka cofg class

--- src/main/java/com/ridesharing/Config/KafkaConfig.java
+++ src/main/java/com/ridesharing/Config/KafkaConfig.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
