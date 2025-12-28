291c3b41aa3cd06af1878c5a3e9348cd0b6b94cb
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-28 18:42:29 +0545

    added method of fimd by status in driver application repo

--- src/main/java/com/ridesharing/Repository/DriverApplicationRepository.java
+++ src/main/java/com/ridesharing/Repository/DriverApplicationRepository.java
@@ -1,12 +1,17 @@
 package com.ridesharing.Repository;
 
 import com.ridesharing.Entities.DriverApplication;
+import com.ridesharing.Entities.DriverStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DriverApplicationRepository
         extends JpaRepository<DriverApplication, Long> {
 
     Optional<DriverApplication> findByUserId(Long userId);
+
+    //  REQUIRED for admin pending list
+    List<DriverApplication> findByStatus(DriverStatus status);
 }
