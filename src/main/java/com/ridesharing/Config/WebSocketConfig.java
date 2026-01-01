c876a1592b10bf6db356d494302a9e459550cfbb
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-01-01 20:56:18 +0545

    removed wb sckt as well

--- src/main/java/com/ridesharing/Config/WebSocketConfig.java
+++ src/main/java/com/ridesharing/Config/WebSocketConfig.java
@@ -1,25 +1,25 @@
-package com.ridesharing.Config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
-
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    // client connect to /ws endpnt
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws") // primary websocket endp
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
-    }
-
-    // message broker for routing message (simple in-memory broker)
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic", "/queue"); // topics for broadcasts
-        registry.setApplicationDestinationPrefixes("/app"); // client sends to /app/*
-    }
-}
+//package com.ridesharing.Config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.*;
+//
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//    // client connect to /ws endpnt
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/ws") // primary websocket endp
+//                .setAllowedOriginPatterns("*")
+//                .withSockJS();
+//    }
+//
+//    // message broker for routing message (simple in-memory broker)
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        registry.enableSimpleBroker("/topic", "/queue"); // topics for broadcasts
+//        registry.setApplicationDestinationPrefixes("/app"); // client sends to /app/*
+//    }
+//}
