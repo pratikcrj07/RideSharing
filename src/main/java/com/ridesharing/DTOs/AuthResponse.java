be7e9ebc36673363ef2109d988ec30ecef57dae3
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-01-10 22:20:33 +0545

    auth res

--- src/main/java/com/ridesharing/DTOs/AuthResponse.java
+++ src/main/java/com/ridesharing/DTOs/AuthResponse.java
@@ -10,6 +10,7 @@ public class AuthResponse {
     private String refreshToken;
     private String tokenType = "Bearer";
 
+
     public AuthResponse(String accessToken, String refreshToken) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
